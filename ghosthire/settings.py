4596aaeb7e13b13ee5c0dd56cde5ec5efe6c5d28
Pooja Va <likeway64@gmail.com>
2025-11-19 09:15:13 +0530

    Update settings.py

--- ghosthire/settings.py
+++ ghosthire/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-for-local")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get("ALLOWED_HOSTS", "localhost").split(",")]
 
 
 # Application definition
