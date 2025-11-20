0cbd3a927a85228cc47587eccbb89552977a7fdc
Pooja Va <likeway64@gmail.com>
2025-11-20 10:33:36 +0530

    Update settings.py

--- ghosthire/settings.py
+++ ghosthire/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-for-local")
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [host.strip() for host in os.environ.get("ALLOWED_HOSTS", "localhost").split(",")]
-
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
 
 # Application definition
 
